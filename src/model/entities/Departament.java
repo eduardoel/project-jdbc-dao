0fac7dc448bf42c1ce508b98091ea02fcadc8653
Eduardo Elly <eduardo_elly@hotmail.com>
2020-03-15 01:47:44 -0300

    Department entity class - Classe de entidade do departamento

---
+++ src/model/entities/Departament.java
@@ -0,0 +1,65 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Departament implements Serializable {
+    
+    private Integer id;
+    private String name;
+    
+    public Departament() {
+    }
+
+    public Departament(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Departament other = (Departament) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Departament{" + "id=" + id + ", name=" + name + '}';
+    }
+    
+}
